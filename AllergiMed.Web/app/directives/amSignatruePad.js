69bf6e5d8c9217863bba4e8f166f4d8ade825d94
Yu Bao <imbaoyu@gmail.com>
2015-09-24 00:09:17 -0400

    fix signature pad

--- AllergiMed.Web/app/directives/amSignatruePad.js
+++ AllergiMed.Web/app/directives/amSignatruePad.js
@@ -1,17 +1,5 @@
 ﻿'use strict';
 app.directive('amSignature', function () {
-    // Adjust canvas coordinate space taking into account pixel ratio,
-    // to make it look crisp on mobile devices.
-    // This also causes canvas to be cleared.
-    function resizeCanvas(canvas) {
-        // When zoomed out to less than 100%, for some very strange reason,
-        // some browsers report devicePixelRatio as less than 1
-        // and only part of the canvas is cleared then.
-        var ratio = Math.max(window.devicePixelRatio || 1, 1);
-        canvas.width = canvas.offsetWidth * ratio;
-        canvas.height = canvas.offsetHeight * ratio;
-        canvas.getContext("2d").scale(ratio, ratio);
-    }
 
     return {
         restrict: 'A',
@@ -21,9 +9,6 @@ app.directive('amSignature', function () {
                     saveButton = element.find("[data-action=save]").get(0),
                     canvas = element.find("canvas").get(0),
                     signaturePad;
-
-                window.onresize = resizeCanvas;
-                resizeCanvas(canvas);
 
                 signaturePad = new SignaturePad(canvas);
 
